7c36b5f934b80ea11107b60d7fa2c291e5d2f1d8
M4darin <101992990+M4darin@users.noreply.github.com>
2023-06-16 18:01:57 +0300

    1.4

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,7 +4,8 @@ public class Application {
     }
     public static void Hello(){
         System.out.println("Hello world!!!");///ddasdsadasdasdasdasda
-
+     int a = 0;
+     int b = a + 1;
     }
 
 
